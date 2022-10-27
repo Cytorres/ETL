c0653a6f8feda2b1044c2754f2f67cceaeeab21b
Cytorres <cynthiatst85@gmail.com>
2022-10-27 16:23:25 -0300

    'salvar'

---
+++ src/drivers/http_requester.py
@@ -0,0 +1,17 @@
+from typing import Dict
+
+import requests
+
+
+class HttpRequester:
+
+    def __init__(self):
+        self.__url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
+
+    def request_from_page(self) -> Dict[int, str]:
+        response = requests.get(self.__url, timeout = 30)
+        return {
+            "status_code": response.status_code,
+            "html": response.text
+        }
+        
